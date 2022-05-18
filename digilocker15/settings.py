6a6081c7c8968387d55c730f2389db95ad8cffde
“Arpit <111arpit1@gmail.com>
2022-05-18 16:40:45 +0530

    fixed

--- digilocker15/settings.py
+++ digilocker15/settings.py
@@ -121,21 +121,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-django_heroku.settings(locals())
-
 STATIC_URL = '/static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+django_heroku.settings(locals())
+
+
 SVG_DIRS=[
     os.path.join(BASE_DIR,'static', 'images')
 ]
